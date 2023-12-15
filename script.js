2917e05ebef84c3c2d60a9db7a5cf0cee6f81e2b
Harshal Patil <96765680+harshalpatil9@users.noreply.github.com>
2023-12-15 12:51:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-const questionsContainer = document.getElementById('questions-container');
-const answerContainer = document.getElementById('answer-container');
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const questionsContainer = document.getElementById('questions-container');
+    const answerContainer = document.getElementById('answer-container');
 
-const questions = [
-    { id: 1, question: 'What is HTML?', answer: 'HTML stands for HyperText Markup Language, and it is the standard markup language for creating web pages.' },
-    { id: 2, question: 'What is CSS?', answer: 'CSS stands for Cascading Style Sheets, and it is used for describing the look and formatting of a document written in HTML.' },
-    // Add more questions as needed
-];
+    const questions = [
+        { id: 1, question: 'What is HTML?', answer: 'HTML stands for HyperText Markup Language, and it is the standard markup language for creating web pages.' },
+        { id: 2, question: 'What is CSS?', answer: 'CSS stands for Cascading Style Sheets, and it is used for describing the look and formatting of a document written in HTML.' },
+        // Add more questions as needed
+    ];
 
-function showQuestions() {
-    questionsContainer.innerHTML = '<h2>Questions:</h2><ul>' +
-        questions.map(question => `<li onclick="showAnswer(${question.id})">${question.question}</li>`).join('') +
-        '</ul>';
-}
+    function showQuestions() {
+        questionsContainer.innerHTML = '<h2>Questions:</h2><ul>' +
+            questions.map(question => `<li>${question.id}. ${question.question}</li>`).join('') +
+            '</ul>';
 
-function showAnswer(id) {
-    const selectedQuestion = questions.find(question => question.id === id);
-    answerContainer.innerHTML = `<h2>${selectedQuestion.question}</h2><p>${selectedQuestion.answer}</p>`;
-    answerContainer.style.display = 'block';
-}
+        // Add event listeners to each question item
+        questions.forEach(question => {
+            const listItem = document.querySelector(`li:nth-child(${question.id})`);
+            listItem.addEventListener('click', () => showAnswer(question.id));
+        });
+    }
 
-showQuestions();
+    function showAnswer(id) {
+        const selectedQuestion = questions.find(question => question.id === id);
+        answerContainer.innerHTML = `<h2>${selectedQuestion.id}. ${selectedQuestion.question}</h2><p>${selectedQuestion.answer}</p>`;
+        answerContainer.style.display = 'block';
+    }
+
+    showQuestions();
+});
